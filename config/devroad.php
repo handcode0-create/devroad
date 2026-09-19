a7a5bce2dc6460e66e1a407ff47700d957189ea8
handcode0-create <handcode0@gmail.com>
2026-09-19 18:12:24 +0200

    feat: centralize supported roadmap technologies

---
+++ config/devroad.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'technologies' => [
+        'laravel' => 'Laravel',
+        'nextjs' => 'Next.js',
+        'react' => 'React',
+        'javascript' => 'JavaScript',
+        'typescript' => 'TypeScript',
+        'php' => 'PHP',
+        'html' => 'HTML',
+        'css' => 'CSS',
+        'tailwind' => 'Tailwind CSS',
+        'node' => 'Node.js',
+        'git' => 'Git',
+        'github' => 'GitHub',
+        'docker' => 'Docker',
+        'mysql' => 'MySQL',
+        'postgresql' => 'PostgreSQL',
+    ],
+];
