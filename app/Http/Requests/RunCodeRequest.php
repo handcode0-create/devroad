780a67ba080f7da1d5ace4ece7505691b4221d69
handcode0-create <handcode0@gmail.com>
2026-09-19 20:29:54 +0200

    feat: accept sandbox commands and files

--- app/Http/Requests/RunCodeRequest.php
+++ app/Http/Requests/RunCodeRequest.php
@@ -18,12 +18,23 @@ class RunCodeRequest extends FormRequest
             'language' => [
                 'required',
                 'string',
-                Rule::in(['php', 'javascript']),
+                Rule::in(['php', 'javascript', 'laravel']),
             ],
             'code' => [
-                'required',
+                'nullable',
                 'string',
                 'max:20000',
+            ],
+            'command' => [
+                'nullable',
+                'string',
+                'max:300',
+            ],
+            'file_path' => [
+                'nullable',
+                'string',
+                'max:180',
+                'regex:/^(?!.*\.\.)(?:[A-Za-z0-9_-]+\/)*[A-Za-z0-9_.-]+$/',
             ],
         ];
     }
