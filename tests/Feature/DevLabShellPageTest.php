6247cdf1fd4197e5c1ab164fc99722ea3d109f3a
handcode0-create <handcode0@gmail.com>
2026-09-20 02:44:06 +0000

    test(devlab): keep shell page covered

--- tests/Feature/DevLabShellPageTest.php
+++ tests/Feature/DevLabShellPageTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Support\HtmlString;
 
 class DevLabShellPageTest extends TestCase
 {
