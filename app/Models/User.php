f7a76582fdd20443d934497ef9bd96373904d593
handcode0-create <handcode0@gmail.com>
2026-09-20 16:43:16 +0000

    feat(sandbox): add user sandbox relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -77,4 +77,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(DevLabProject::class);
     }
+
+    public function sandboxProjects(): HasMany
+    {
+        return $this->hasMany(SandboxProject::class);
+    }
 }
