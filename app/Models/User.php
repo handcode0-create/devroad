dffc4128ecf1d638ac90d81a408ecd1c145152d0
handcode0-create <handcode0@gmail.com>
2026-09-23 23:12:02 +0000

    feat(onboarding): build learning profile flow

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,23 +2,18 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that should be mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -32,21 +27,11 @@ class User extends Authenticatable
         'light_mode',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -93,5 +78,10 @@ class User extends Authenticatable
     public function sandboxProjects(): HasMany
     {
         return $this->hasMany(SandboxProject::class);
+    }
+
+    public function learningProfile(): HasOne
+    {
+        return $this->hasOne(UserLearningProfile::class);
     }
 }
