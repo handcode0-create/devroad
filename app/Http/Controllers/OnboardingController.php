f8f8123df83787908fc52c3a7d6d082171b021f4
handCode <handcode0@gmail.com>
2026-09-25 11:34:18 +0000

    chore(onboarding): remove unused database import

--- app/Http/Controllers/OnboardingController.php
+++ app/Http/Controllers/OnboardingController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\UserLearningProfile;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
 use Inertia\Inertia;
 use Inertia\Response;
